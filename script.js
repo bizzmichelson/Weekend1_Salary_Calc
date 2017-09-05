8e07086bdc2c2886762aba3280896d87b92d8c64
bizzmichelson <bizz.michelson@gmail.com>
2017-09-04 23:01:31 -0500

    Final commit

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-var employeeInfo = [];
+var  employeeInfo = [];
 var yearlyCosts = 0;
 var monthlyTotals = 0;
 
 //store the information
-// ** WHy do the fields get cleared?
+// ** Why do the fields get cleared?
 
 $(document).ready(onReady);
 function onReady() {
     console.log('ready');
 
     //event listeners
-
-    $('#submitButton').on('click', addItem);
-    $('#submitButton').on('click', AppendDOM);
-    $('#submitButton').on('click', addCosts);
-    //a'Submit' button should collect the form information,
-    // $('#submit').on('click', AppendInfo);
-    // $('#submit').on('click', ClearInput);
-    
-
+    $('#submitButton').click(handleClick);
+    //a'Submit' button should collect the form information
 }
 
-function addItem() {
+
+function handleClick() {
     new Item( $('#firstNameIn').val(), $('#lastNameIn').val(), $('#idNumberIn').val(), $('#jobTitleIn').val(), $('#annualSalaryIn').val());
-   $('#firstNameIn').val('');
-   $('#lastNameIn').val('');
-   $('#idNumberIn').val('');
-   $('#jobTitleIn').val('');
-   $('#annualSalaryIn').val('');   
+     $('#firstNameIn').val('');
+     $('#lastNameIn').val('');
+     $('#idNumberIn').val('');
+     $('#jobTitleIn').val('');
+     $('#annualSalaryIn').val('');   
 }
 
 function Item (firstName, lastName, idNumber, jobTitle, annualSalary) {
@@ -36,28 +30,35 @@ function Item (firstName, lastName, idNumber, jobTitle, annualSalary) {
     this.idNumber = idNumber;
     this.jobTitle = jobTitle;
     this.annualSalary = annualSalary;
+    appendDOM(this);
+    addCosts(this);
     employeeInfo.push(this);
 }
 //Create an application that records employee salaries and adds salaries up to report monthly costs. 
 
-function AppendDOM() {
-    //var $employeeArticle = $('<article>').data('employeeInfo');
-
-    $('#employee-display').append(
-        '<p>' + $('#firstNameIn').val() + ' ' + $('#lastNameIn').val() + '</p>'
+function appendDOM(employeeObject) {
+    
+    var addEmployeeName = '<p>' + employeeObject.firstName + ' ' + employeeObject.lastName + '</p>';
+    var addEmployeeId = '<p>' + employeeObject.idNumber + '</p>';
+    var addEmployeejobTitle = '<p>' + employeeObject.jobTitle + '</p>';
+    var addEmployeeAnnualSalary = '<p>' + employeeObject.annualSalary + '</p>';
+    
+    $('main').append(
+        '<div>' + addEmployeeName + addEmployeeId + addEmployeejobTitle + addEmployeeAnnualSalary + '</div>'
     );
-
+    
     console.log('append');
 }
 
-function addCosts (employeeInfo) {
+function addCosts(employeeObject) {
+
     console.log('addCosts');
 
-    yearlyCosts += Number(employeeInfo.annualSalary);
+    yearlyCosts += employeeObject.annualSalary;
 
     monthlyTotals = Math.round(yearlyCosts / 12);
 
-    $('.total').html(monthlyTotals);
+    $('.total').html('<h2>'+ monthlyTotals + '</h2>');
 }
 
 // A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. Using the stored information, calculate monthly costs and append this to the to DOM.
